c7a31f5d0a76b9a56c603579fee09ed329ae99b3
Erik Laucks <phisigmapi314@gmail.com>
2018-04-02 19:43:23 -0400

    Cleaned code and comments

--- NetworkLayer.java
+++ NetworkLayer.java
@@ -1,41 +1,35 @@
 import java.util.*;
+
 /**
- * A class which represents the transoprt layer for both sender and receiver.
+ * A class which represents the network layer for both sender and receiver.
  */
+public class NetworkLayer {
+  float lossProbability; //probability of losing a packet
+  float currProbability; //probability of corrupting a packet
+  Timeline tl;
+  Random ran; //random number generator for losing packets.
 
-public class NetworkLayer
-{
-    float lossProbability; //probablity of losing a packet
-    float currProbability; //probability of curropting a packet
-    Timeline tl; 
-    Random ran; //random number generator for losing packets.
+  public NetworkLayer(float lp, float cp,Timeline tl) {
+    lossProbability = lp;
+    currProbability = cp;
+    this.tl = tl;
+    ran = new Random();
+  }
 
-    public NetworkLayer(float lp, float cp,Timeline tl)
-    {
-        lossProbability=lp;
-        currProbability=cp;
-        this.tl=tl;
-        ran = new Random();
+  //sending packet if it is not lost, and corrupting it if necessary.
+  public void sendPacket(Packet pkt, int to) {
+    if(ran.nextDouble() < lossProbability) {
+      if(NetworkSimulator.DEBUG > 1)
+        System.out.println("Packet seq:" + pkt.getSeqnum() + " ack: " + pkt.getAcknum() + " lost");
+      return;
     }
-
-    //sending packet if it is not lost, and curropting it if necessary.
-    public void sendPacket(Packet pkt, int to)
-    {
-        if(ran.nextDouble()<lossProbability)
-        {
-            if(NetworkSimulator.DEBUG>1)
-                System.out.println("Packet seq:" + pkt.getSeqnum() + " ack: " + pkt.getAcknum() + " lost");
-            return;    
-        }
-        if(ran.nextDouble()<currProbability)
-        {
-            if(NetworkSimulator.DEBUG>1)
-                System.out.println("Packet seq:" + pkt.getSeqnum() + " ack: " + pkt.getAcknum() + " curropted");
-            pkt.corrupt();
-        }
-        if(NetworkSimulator.DEBUG>1)
-            System.out.println("Packet seq:" + pkt.getSeqnum() + " ack: " + pkt.getAcknum() + " sent");
-        tl.createArriveEvent(pkt,to);
+    if(ran.nextDouble() < currProbability) {
+      if(NetworkSimulator.DEBUG > 1)
+        System.out.println("Packet seq:" + pkt.getSeqnum() + " ack: " + pkt.getAcknum() + " curropted");
+      pkt.corrupt();
     }
-
+    if(NetworkSimulator.DEBUG > 1)
+      System.out.println("Packet seq:" + pkt.getSeqnum() + " ack: " + pkt.getAcknum() + " sent");
+    tl.createArriveEvent(pkt, to);
+  }
 }
--- SenderTransport.java
+++ SenderTransport.java
@@ -1,60 +1,48 @@
-
 import java.util.ArrayList;
+
 /**
- * A class which represents the receiver transport layer
+ * A class which represents the sender transport layer
  */
-public class SenderTransport
-{
-    private NetworkLayer nl;
-    private Timeline tl;
-    private int n;
-    private int mss;
-    private boolean bufferingPackets;
+public class SenderTransport {
+  private NetworkLayer nl;
+  private Timeline tl;
+  private int n;
+  private int mss;
+  private boolean bufferingPackets;
 
-    public SenderTransport(NetworkLayer nl){
-        this.nl=nl;
-        initialize();
+  public SenderTransport(NetworkLayer nl) {
+    this.nl = nl;
+    initialize();
+  }
 
-    }
+  public void initialize() {
+  }
 
-    public void initialize()
-    {
-    }
+  public void sendMessage(Message msg) {
+  }
 
-    public void sendMessage(Message msg)
-    {
-    }
+  public void receiveMessage(Packet pkt) {
+  }
 
-    public void receiveMessage(Packet pkt)
-    {
-    }
+  public void timerExpired() {
+  }
 
-    public void timerExpired()
-    { 
-    }
+  public void setTimeLine(Timeline tl) {
+    this.tl = tl;
+  }
 
-    public void setTimeLine(Timeline tl)
-    {
-        this.tl=tl;
-    }
+  public void setWindowSize(int n) {
+    this.n = n;
+  }
 
-    public void setWindowSize(int n)
-    {
-        this.n=n;
-    }
-    
-    public void setMSS(int n)
-    {
-        this.n=n;
-    }
-    
+  public void setMSS(int n) {
+    this.n = n;
+  }
 
-    public void setProtocol(int n)
-    {
-        if(n>0)
-            bufferingPackets=true;
-        else
-            bufferingPackets=false;
-    }
-
+  public void setProtocol(int n) {
+    if(n > 0)
+      bufferingPackets = true;
+    else
+      bufferingPackets = false;
+  }
 }
